80a937b2c0059d21131343ce924b38852d5cc14b
de-uchi <urchin2x@gmail.com>
2024-12-07 17:06:13 +0900

    added insertion for popups

--- docs/js/popup.js
+++ docs/js/popup.js
@@ -1,6 +1,9 @@
 $(function() {
     const $body = $('body');
     const $popupWindow = $('#js-popupWindow');
+    const $popupImage = $('#js-popupImage');
+    const $popupTitle = $('#js-popupTitle');
+    const $popupDetail = $('#js-popupDetail');
 
     let scrollTop;
 
@@ -12,12 +15,18 @@ $(function() {
             top: -scrollTop
         });
         $popupWindow.removeClass('p-dispNone');
+
+        $popupImage.attr('src', $(this).attr('data-src'));
+        $popupTitle.append($(this).attr('data-title'));
+        $popupDetail.append($(this).attr('data-detail'));
     });
 
     $('#js-closePopupWindow, #js-closePopupWindowButton').on('click', function(e) {
         $body.removeClass('fixed');
         $(window).scrollTop(scrollTop);
         $popupWindow.addClass('p-dispNone');
+        $popupTitle.empty();
+        $popupDetail.empty();
     });
 
     $('#js-popupWindowBody').on('click', function(e) {
